2c1bbb7611193a805ed2cbf841366c37146b0654
lilouuuuuuu <lilou.pedro@etu.iut-tlse3.fr>
2024-03-08 11:10:21 +0100

    liens index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>Gestion d'un cabinet médical</title>
-    <link rel="shortcut icon" href="/Donnees/patientele_icon.ico" />
-    <link rel="stylesheet" href="/CSS/base.css">
-    <link rel="stylesheet" href="/CSS/index.css">
+    <link rel="shortcut icon" href="Donnees/patientele_icon.ico" />
+    <link rel="stylesheet" href="CSS/base.css">
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
 <body>
 	<div class="container" id="container">
@@ -22,7 +22,7 @@
 			<div class="overlay">
 				<div class="overlay-panel overlay-right">
 					<h1>Gestion d'un cabinet médical</h1>
-					<img src="/Donnees/doctor.png" alt="docteur">
+					<img src="Donnees/doctor.png" alt="docteur">
 				</div>
 			</div>
 		</div>
