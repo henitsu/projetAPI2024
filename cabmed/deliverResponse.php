7f4ac709b931979a4675a4ca2145e7ff6ca518e3
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2024-03-27 10:33:17 +0100

    Update deliverResponse.php

--- cabmed/deliverResponse.php
+++ cabmed/deliverResponse.php
@@ -1,10 +1,16 @@
 <?php
     // Envoi de la réponse au Client
     function deliverResponse($status_code, $status_message, $data=null){
-        $methods = 'GET, POST, PATCH, DELETE';
+        $methods = 'GET, POST, PATCH, DELETE, OPTIONS';
         // Paramétrage de l'entête HTTP
-        http_response_code($status_code);
-        header("Content-Type: application/json; charset=utf-8");
+        $headers = 'Content-Type, Authorization';
+        http_response_code($status_code); // Utilise un message standardisé en fonction du code HTTP
+        // header("HTTP/1.1 $status_code $status_message"); // Permet de personnaliser le message associé au code HTTP
+        header("Content-Type:application/json; charset=utf-8"); // Indique au client le format de la réponse
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: $methods");
+        header("Access-Control-Allow-Headers: $headers");
+
         /// GESTION DES CORS DANS .htaccess
         $response['status_code'] = $status_code;
         $response['status_message'] = $status_message;
