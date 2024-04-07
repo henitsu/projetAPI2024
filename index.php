b95e4b9e7fad77081efd4c887c794e22cc16ddae
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2024-04-07 03:46:49 +0200

    Update authentication message in index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 				<h1>Authentification</h1>
 				<input name="identifiant" type="text" placeholder="Identifiant" />
 				<input name="mot_de_passe" type="password" placeholder="Mot de passe" />
-				<p>Mot de passe oublié ? Consultez le README</p>
+				<p>Entrez n'importe quel mot de passe</p>
 				<button>Se connecter</button>
 			</form>
 		</div>
